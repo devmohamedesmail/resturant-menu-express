698940e358c9f734546ee032a18584c85aea1920
Mohamed Esmail <dev.mohamed.esmail@gmail.com>
2025-02-04 10:38:01 +0400

    update

--- server/routes/admin/orders.js
+++ server/routes/admin/orders.js
@@ -3,8 +3,31 @@ const router = express.Router();
 const Order = require("../../models/Order")
 
 router.get('/show/orders', async(req,res)=>{
-   const orders = await Order.find().sort({ createAt: -1 });
+   const orders = await Order.find({ status: "prepare" }).sort({ createdAt: -1 });
    res.render('admin/pages/orders.ejs',{orders});
 })
+
+
+
+
+
+
+
+router.get('/change/order/status/:_id', async (req,res)=>{
+    const id  = req.params._id;
+    await Order.findByIdAndUpdate(id,{status:"completed"});
+    res.redirect('/odersroutes/show/orders')
+})
+
+
+
+
+
+
+router.get('/show/completed/orders', async(req,res)=>{
+    const orders = await Order.find({ status: "completed" }).sort({ createdAt: -1 });
+    res.render('admin/pages/completed-orders.ejs',{orders});
+ })
+
 
 module.exports = router
